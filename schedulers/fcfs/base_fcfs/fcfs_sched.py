3e787e780c0ce86f3d6718e4e63e8bb7c6def1be
naikelin <nicolas.nunez2@mail.udp.cl>
2023-09-17 23:59:23 -0300

    fix temporal a dependencias

--- schedulers/fcfs/base_fcfs/fcfs_sched.py
+++ schedulers/fcfs/base_fcfs/fcfs_sched.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 from procset import ProcSet
 from itertools import islice
 
@@ -7,6 +10,12 @@ class Fcfs_sched(BatsimScheduler):
     def __init__(self, options):
         super().__init__(options)
         self.logger.info("FCFS with Dependencies init")
+
+    def load_dependencies(self):
+        with open("/home/nk/memoria/sched-sim/workloads/hash_dependencies.json", "r") as file:
+            self.job_dependencies = json.load(file)
+            
+        self.job_dependencies = {int(k): v for k, v in self.job_dependencies.items()}
 
     def onSimulationBegins(self):
         self.nb_completed_jobs = 0
@@ -18,11 +27,31 @@ class Fcfs_sched(BatsimScheduler):
         self.computing_machines = ProcSet()
         self.idle_machines = ProcSet((0, self.bs.nb_compute_resources - 1))
 
+        self.load_dependencies()
+
     def job_is_ready(self, job):
         """Check if all dependencies of the job have been completed."""
-        if hasattr(job, 'dependencies'):
-            return all(dep in self.completed_jobs for dep in job.dependencies)
-        return True  # If job doesn't have dependencies, consider it ready
+        # Extraer el ID del trabajo usando regex
+        match = re.search(r'!(\d+)$', job.id)
+        if not match:
+            print(f"Job {job.id} does not have the expected format.")
+            return False  # Si no se encuentra el formato esperado, considerar que el trabajo no está listo
+            
+        job_id = int(match.group(1))  # Convertir el ID extraído a entero
+
+        # Verificar si el trabajo tiene dependencias en el atributo 'job_dependencies'
+        if job_id in self.job_dependencies:
+            dependencies_met = [dep for dep in self.job_dependencies[job_id] if dep in self.completed_jobs]
+            dependencies_not_met = [dep for dep in self.job_dependencies[job_id] if dep not in self.completed_jobs]
+
+            print(f"Checking job: {job.id}")
+            print(f"Dependencies of the job: {self.job_dependencies[job_id]}")
+            print(f"Met dependencies: {dependencies_met}")
+            print(f"Unmet dependencies: {dependencies_not_met}")
+
+            return len(dependencies_not_met) == 0
+
+        return True  # Si el trabajo no tiene dependencias en 'job_dependencies', considerarlo listo
 
     def scheduleJobs(self):
         print('\n\nopen_jobs = ', [job.id for job in self.open_jobs])
@@ -33,6 +62,7 @@ class Fcfs_sched(BatsimScheduler):
 
         # Filter out jobs that are ready to run (all dependencies met)
         ready_jobs = [job for job in self.open_jobs if self.job_is_ready(job)]
+        print('ready_jobs can be scheduled! = ', [job.id for job in ready_jobs])
 
         for job in ready_jobs:
             nb_res_req = job.requested_resources
@@ -55,17 +85,28 @@ class Fcfs_sched(BatsimScheduler):
         self.bs.execute_jobs(scheduled_jobs)
 
     def onJobSubmission(self, job):
+        print(f"Job {job.id} submitted")
         if job.requested_resources > self.bs.nb_compute_resources:
             self.bs.reject_jobs([job])  # This job requests more resources than the machine has
         else:
             self.open_jobs.append(job)
 
     def onJobCompletion(self, job):
+        print(f"Job {job.id} completed!")
+
+        # Extraer el ID numérico del trabajo usando regex
+        match = re.search(r'!(\d+)$', job.id)
+        if match:
+            numeric_job_id = int(match.group(1))
+            self.completed_jobs.add(numeric_job_id)  # Mark job as completed using the numeric ID
+        else:
+            print(f"Warning: Job ID {job.id} does not have the expected format.")
+
         self.idle_machines |= job.allocation
         self.computing_machines -= job.allocation
-        self.completed_jobs.add(job.id)  # Mark job as completed
+    
+    def onDeadlock(self):
+        self.bs.end_simulation()
 
     def onNoMoreEvents(self):
         self.scheduleJobs()
-
-
--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
 { kapack ? import
-    ( fetchTarball "https://github.com/oar-team/nur-kapack/archive/master.tar.gz")
+    (fetchTarball {
+      url = "https://github.com/oar-team/nur-kapack/archive/8c2f3d907db117229fa11c95c9342f9026daf9c2.tar.gz";
+      # Asegúrate de obtener y usar la SHA256 correcta para este commit
+      sha256 = "0n53jj3kw1vcs2x08a3sad744c3j1699d3z0nkbzljhgyhwh8li0";
+    })
   {}
 }:
 
@@ -10,6 +14,7 @@ let
     experiment_env = mkShell rec {
       name = "experiment_env";
       buildInputs = [
+        kapack.simgrid
         kapack.batsim
         kapack.batsched
         kapack.pybatsim
